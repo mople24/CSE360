7e45237bce0ca634b6d9a8272c313c52dea8310f
Martin <mople@asu.edu>
2020-10-05 21:32:03 -0700

    Part 2 of assignment

--- CSE360Assignment/src/cse360assignment02/AddingMachine.java
+++ CSE360Assignment/src/cse360assignment02/AddingMachine.java
@@ -2,33 +2,33 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
-  StringBuffer history;
+  private String history;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
-    history = new StringBuffer("0");
+    history = "0"; //used to return in toString function
   }
   
   public int getTotal () {
-    return total;
+    return total; //returns the total of calculations
   }
   
   public void add (int value) {
-	  total = value + total;
-	  history.append("+" + value);
-	  
+	  total = value + total; //adds value to total
+	  history = history + "+" + value; //uses history as a toString
   }
 
   public void subtract (int value) {
-	  total = value + total;
-	  history.append("-" + value);
+	  total = value - total; //subtracts value to total
+	  history = history + "-" + value; //uses history as a toString
   }
 
   public String toString () {
-    return history.toString();
+    return history; //returns history
   }
 
   public void clear() {
-	  history = new StringBuffer("0");
+	  System.gc(); //clears memory
   }
+  
 }
